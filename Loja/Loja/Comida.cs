20b49e3a76c77d98b46b1750f7f8e01b38bafff4
Gustavo <gusteitiom@hotmail.com>
2018-05-17 22:08:19 -0300

    Fim da criação das classes

---
+++ Loja/Loja/Comida.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja
+{
+    class Comida : Produto
+    {
+        private int porcentImposto;
+        protected override int getPorcentagemImposto()
+        {
+            return 0;
+        }
+    }
+}
